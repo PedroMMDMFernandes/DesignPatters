1af5aefe44f08ca433bae6d6968134411b6c0373
PedroVSC <pedro.fernandes.gmr@gmail.com>
2021-10-01 11:25:17 +0100

    feat: implement bridge Design Pattern

--- Structural/bridge/bridge.cpp
+++ Structural/bridge/bridge.cpp
@@ -1,9 +1,91 @@
 #include "bridge.hpp"
 
-Bridge::Bridge(/* args */)
+namespace BridgeDP
 {
-}
+    ImplementationInterface::ImplementationInterface()
+    {
+    }
+    
+    ImplementationInterface::~ImplementationInterface()
+    {
+    }
 
-Bridge::~Bridge()
-{
+
+    Abstraction::Abstraction()
+    {
+    }
+    
+    Abstraction::~Abstraction()
+    {
+    }
+
+    ConcreteImplementation1::ConcreteImplementation1()
+    {
+    }
+    
+    ConcreteImplementation1::~ConcreteImplementation1()
+    {
+    }
+
+
+    ConcreteImplementation2::ConcreteImplementation2()
+    {
+    }
+    
+    ConcreteImplementation2::~ConcreteImplementation2()
+    {
+    }
+
+    std::string ConcreteImplementation1::method() const
+    {
+        return "ConcreteImplementation1 Method1 Implementation";
+    }
+
+    std::string ConcreteImplementation2::method() const    
+    {
+        return "ConcreteImplementation2 Method2 Implementation";
+    }
+
+    RefinedAbstraction::RefinedAbstraction(ImplementationInterface* impl)
+    {
+        this->impl = impl;
+    }
+    
+    RefinedAbstraction::~RefinedAbstraction()
+    {
+        delete this->impl;
+    }
+
+    std::string RefinedAbstraction::DoOperation(void) const
+    {
+        return "Refined Operation with " + impl->method();
+    }
+
+    Main::Main()
+    {
+    }
+    
+    Main::~Main()
+    {
+    }
+
+    void Main::execute()
+    {
+        ImplementationInterface* conImpl = new ConcreteImplementation1;
+        
+        Abstraction* abst = new RefinedAbstraction(conImpl);
+
+        std::cout<<abst->DoOperation()<<std::endl;
+        
+        delete conImpl;
+        delete abst;
+
+        conImpl = new ConcreteImplementation2;
+        abst = new RefinedAbstraction(conImpl);
+        
+        std::cout<<abst->DoOperation()<<std::endl;
+
+        delete conImpl;
+        delete abst;
+    }
 }
--- Structural/bridge/bridge.hpp
+++ Structural/bridge/bridge.hpp
@@ -1,9 +1,105 @@
-class Bridge
+#include <iostream>
+
+namespace BridgeDP
 {
-private:
-    /* data */
-public:
-    Bridge(/* args */);
-    ~Bridge();
-};
+    class ImplementationInterface
+    {
+    public:
+        ImplementationInterface();
+        ImplementationInterface(ImplementationInterface &&) = default;
+        ImplementationInterface(const ImplementationInterface &) = default;
+        ImplementationInterface &operator=(ImplementationInterface &&) = default;
+        ImplementationInterface &operator=(const ImplementationInterface &) = default;
+        ~ImplementationInterface();
+
+        virtual std::string method() const = 0;
+    
+    private:
+        
+    };
+
+    class ConcreteImplementation1 : public ImplementationInterface
+    {
+    public:
+        ConcreteImplementation1();
+        ConcreteImplementation1(ConcreteImplementation1 &&) = default;
+        ConcreteImplementation1(const ConcreteImplementation1 &) = default;
+        ConcreteImplementation1 &operator=(ConcreteImplementation1 &&) = default;
+        ConcreteImplementation1 &operator=(const ConcreteImplementation1 &) = default;
+        ~ConcreteImplementation1();
+
+        std::string method() const override;
+    
+    private:
+        
+    };
+
+    class ConcreteImplementation2: public ImplementationInterface 
+    {
+    public:
+        ConcreteImplementation2();
+        ConcreteImplementation2(ConcreteImplementation2 &&) = default;
+        ConcreteImplementation2(const ConcreteImplementation2 &) = default;
+        ConcreteImplementation2 &operator=(ConcreteImplementation2 &&) = default;
+        ConcreteImplementation2 &operator=(const ConcreteImplementation2 &) = default;
+        ~ConcreteImplementation2();
+
+        std::string method() const override;
+    private:
+        
+    };
+  
+    
+    class Abstraction
+    {
+    public:
+        Abstraction();
+        Abstraction(Abstraction &&) = default;
+        Abstraction(const Abstraction &) = default;
+        Abstraction &operator=(Abstraction &&) = default;
+        Abstraction &operator=(const Abstraction &) = default;
+        ~Abstraction();
+
+        virtual std::string DoOperation(void) const = 0;
+    
+    protected:
+        ImplementationInterface* impl;
+        
+    };
+
+    class RefinedAbstraction:  public Abstraction
+    {
+    public:
+        RefinedAbstraction(ImplementationInterface* impl);
+        RefinedAbstraction(RefinedAbstraction &&) = default;
+        RefinedAbstraction(const RefinedAbstraction &) = default;
+        RefinedAbstraction &operator=(RefinedAbstraction &&) = default;
+        RefinedAbstraction &operator=(const RefinedAbstraction &) = default;
+        ~RefinedAbstraction();
+
+        std::string DoOperation(void) const override;
+    
+    private:
+        
+    };
+    
+
+
+    class Main
+    {
+    public:
+        Main();
+        Main(Main &&) = default;
+        Main(const Main &) = default;
+        Main &operator=(Main &&) = default;
+        Main &operator=(const Main &) = default;
+        ~Main();
+
+        void execute();
+    
+    private:
+        
+    };
+}
+
 
