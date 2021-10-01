6bec49e8729c548a6fc69a06e0ef9333798f1895
PedroVSC <pedro.fernandes.gmr@gmail.com>
2021-10-01 16:53:06 +0100

    feat: implement facade DP

--- Structural/facade/facade.cpp
+++ Structural/facade/facade.cpp
@@ -1,10 +1,58 @@
 #include "facade.hpp"
 
-Facade::Facade(/* args */)
+namespace FacadeDP
 {
-}
+    Facade::Facade(Subsystem1* subsystem1, Subsystem2* subsystem2)
+    {
+        this->subsystem1 = subsystem1 ? nullptr : new Subsystem1;
+        this->subsystem2 = subsystem2 ? nullptr : new Subsystem2;
+    }
+    
+    Facade::~Facade()
+    {
+        delete this->subsystem1;
+        delete this->subsystem2;
+    }
 
-Facade::~Facade()
-{
+    std::string Facade::CallSubsystemOperation() const
+    {
+        return "Facade_Operation " + this->subsystem1->CallOperation() + this->subsystem2->CallOperation();  
+    }
+
+    Subsystem1::Subsystem1()
+    {
+    }
+    
+    Subsystem1::~Subsystem1()
+    {
+    }
+
+    std::string Subsystem1::CallOperation() const
+    {
+        return "Subsystem1_Operation";  
+    }
+
+    Subsystem2::Subsystem2()
+    {
+    }
+    
+    Subsystem2::~Subsystem2()
+    {
+    }
+
+    std::string Subsystem2::CallOperation() const
+    {
+        return "Subsystem2_Operation";
+    }
+
+    void Execute()
+    {
+        
+        Facade* facade = new Facade;
+
+        std::cout<<facade->CallSubsystemOperation()<<std::endl;
+
+        delete facade;
+    }
 }
 
--- Structural/facade/facade.hpp
+++ Structural/facade/facade.hpp
@@ -1,9 +1,59 @@
-class Facade
-{
-private:
-    /* data */
-public:
-    Facade(/* args */);
-    ~Facade();
-};
 
+#include <iostream>
+
+namespace FacadeDP
+{
+    class Subsystem1
+    {
+    public:
+        Subsystem1();
+        Subsystem1(Subsystem1 &&) = default;
+        Subsystem1(const Subsystem1 &) = default;
+        Subsystem1 &operator=(Subsystem1 &&) = default;
+        Subsystem1 &operator=(const Subsystem1 &) = default;
+        ~Subsystem1();
+
+        std::string CallOperation() const;
+    
+    private:
+        
+    };
+    
+
+
+    class Subsystem2
+    {
+    public:
+        Subsystem2();
+        Subsystem2(Subsystem2 &&) = default;
+        Subsystem2(const Subsystem2 &) = default;
+        Subsystem2 &operator=(Subsystem2 &&) = default;
+        Subsystem2 &operator=(const Subsystem2 &) = default;
+        ~Subsystem2();
+
+        std::string CallOperation() const;
+    
+    private:
+        
+    };
+    
+    class Facade
+    {
+    public:
+        Facade(Subsystem1* subsystem1 = nullptr, Subsystem2* subsystem2 = nullptr);
+        Facade(Facade &&) = default;
+        Facade(const Facade &) = default;
+        Facade &operator=(Facade &&) = default;
+        Facade &operator=(const Facade &) = default;
+        ~Facade();
+
+        std::string CallSubsystemOperation() const;
+    
+    private:
+        Subsystem1* subsystem1;
+        Subsystem2* subsystem2;
+    };
+    
+    void Execute();
+
+}
