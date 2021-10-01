5f828c8a8a8c045632e9bbc50a02b012ac8bb13e
PedroVSC <pedro.fernandes.gmr@gmail.com>
2021-10-01 16:52:53 +0100

    feat: implement decorator DP

--- Structural/decorator/decorator.cpp
+++ Structural/decorator/decorator.cpp
@@ -1,10 +1,101 @@
 #include "decorator.hpp"
 
-Decorator::Decorator(/* args */)
+namespace DecoratorDP
 {
-}
+    ComponentInterface::ComponentInterface()
+    {
+    }
+    
+    ComponentInterface::~ComponentInterface()
+    {
+    }
+        
+    ConcreteComponent::ConcreteComponent()
+    {
+    }
+    
+    ConcreteComponent::~ConcreteComponent()
+    {
+    }
 
-Decorator::~Decorator()
-{
-}
+    std::string ConcreteComponent::Execute() const
+    {
+        return "ConcreteComponent";
+    }
 
+    BaseDecorator::BaseDecorator(ComponentInterface* compInt)
+    {
+        this->compInt = compInt;
+    }
+    
+    BaseDecorator::~BaseDecorator()
+    {
+    }
+
+    std::string BaseDecorator::Execute() const
+    {
+        
+        return this->compInt->Execute();
+    }
+
+    ConcreteDecorator1::ConcreteDecorator1(ComponentInterface* conImpl)
+    :BaseDecorator(conImpl)
+    {
+    }
+    
+    ConcreteDecorator1::~ConcreteDecorator1()
+    {
+    }
+
+    std::string ConcreteDecorator1::Execute() const
+    {
+        
+        return  "ConcreteDecorator1 " + BaseDecorator::Execute();
+    }
+
+    ConcreteDecorator2::ConcreteDecorator2(ComponentInterface* conImpl)
+    :BaseDecorator(conImpl)
+    {        
+    }
+    
+    ConcreteDecorator2::~ConcreteDecorator2()
+    {
+    }
+
+    std::string ConcreteDecorator2::Execute() const
+    {
+        
+        return "ConcreteDecorator2 " + BaseDecorator::Execute();
+    }
+
+    void execute()
+    {
+        ComponentInterface* conComp = new ConcreteComponent();
+        ComponentInterface* conDec1= new ConcreteDecorator1(conComp);
+
+        std::cout<<conComp->Execute()<<std::endl;
+        std::cout<<conDec1->Execute()<<std::endl;
+
+        ComponentInterface* conDec2= new ConcreteDecorator2(conDec1);
+
+        std::cout<<conDec2->Execute()<<std::endl;
+
+        delete conComp;
+        delete conDec1;
+        delete conDec2;
+    }
+
+    void executeStatic()
+    {
+        ConcreteComponent conComp;
+        ConcreteDecorator1 conDec1(&conComp);
+
+        std::cout<<conComp.Execute()<<std::endl;
+        std::cout<<conDec1.Execute()<<std::endl;
+
+        ConcreteDecorator2 conDec2=(&conDec1);
+
+        std::cout<<conDec2.Execute()<<std::endl;
+    }
+    
+}
--- Structural/decorator/decorator.hpp
+++ Structural/decorator/decorator.hpp
@@ -1,9 +1,88 @@
-class Decorator
-{
-private:
-    /* data */
-public:
-    Decorator(/* args */);
-    ~Decorator();
-};
+#include <iostream>
 
+namespace DecoratorDP
+{
+    class ComponentInterface
+    {
+    public:
+        ComponentInterface();
+        ComponentInterface(ComponentInterface &&) = default;
+        ComponentInterface(const ComponentInterface &) = default;
+        ComponentInterface &operator=(ComponentInterface &&) = default;
+        ComponentInterface &operator=(const ComponentInterface &) = default;
+        ~ComponentInterface();
+
+        virtual std::string Execute() const = 0;
+    
+    private:
+        
+    };
+
+    class ConcreteComponent: public ComponentInterface
+    {
+    public:
+        ConcreteComponent();
+        ConcreteComponent(ConcreteComponent &&) = default;
+        ConcreteComponent(const ConcreteComponent &) = default;
+        ConcreteComponent &operator=(ConcreteComponent &&) = default;
+        ConcreteComponent &operator=(const ConcreteComponent &) = default;
+        ~ConcreteComponent();
+
+        std::string Execute() const;
+    
+    private:
+        
+    };
+
+    class BaseDecorator: public ComponentInterface
+    {
+    public:
+        BaseDecorator(ComponentInterface* compInt);
+        BaseDecorator(BaseDecorator &&) = default;
+        BaseDecorator(const BaseDecorator &) = default;
+        BaseDecorator &operator=(BaseDecorator &&) = default;
+        BaseDecorator &operator=(const BaseDecorator &) = default;
+        ~BaseDecorator();
+
+        virtual std::string Execute() const override;
+    
+    protected:
+        ComponentInterface* compInt;
+        
+    };
+    
+    class ConcreteDecorator1: public BaseDecorator 
+    {
+    public:
+        ConcreteDecorator1(ComponentInterface* conImpl);
+        ConcreteDecorator1(ConcreteDecorator1 &&) = default;
+        ConcreteDecorator1(const ConcreteDecorator1 &) = default;
+        ConcreteDecorator1 &operator=(ConcreteDecorator1 &&) = default;
+        ConcreteDecorator1 &operator=(const ConcreteDecorator1 &) = default;
+        ~ConcreteDecorator1();
+        
+        std::string Execute() const override;
+
+    private:
+        ComponentInterface* conImpl;
+    };
+
+    class ConcreteDecorator2: public BaseDecorator 
+    {
+    public:
+        ConcreteDecorator2(ComponentInterface* conImpl);
+        ConcreteDecorator2(ConcreteDecorator2 &&) = default;
+        ConcreteDecorator2(const ConcreteDecorator2 &) = default;
+        ConcreteDecorator2 &operator=(ConcreteDecorator2 &&) = default;
+        ConcreteDecorator2 &operator=(const ConcreteDecorator2 &) = default;
+        ~ConcreteDecorator2();
+        
+        std::string Execute() const override;
+
+    private:
+        ComponentInterface* conImpl;
+    };
+
+    void execute();
+    void executeStatic();
+}
