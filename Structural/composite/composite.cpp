6416d8562fa88d5ec92b0a709612a8fc5436350e
PedroVSC <pedro.fernandes.gmr@gmail.com>
2021-10-01 11:25:09 +0100

    feat: implement composite Design Pattern

--- Structural/composite/composite.cpp
+++ Structural/composite/composite.cpp
@@ -1,9 +1,129 @@
 #include "composite.hpp"
 
-Composite::Composite(/* args */)
+namespace CompositeDP
 {
-}
+    Component::Component()
+    {
+    }
+    
+    Component::~Component()
+    {
+    }
 
-Composite::~Composite()
-{
+    void Component::SetParent(Component *parent) 
+    {
+        this->parent = parent;
+    }
+
+    Component* Component::GetParent(void) const
+    {
+        return this->parent;
+    }
+
+    bool Component::IsParent() const
+    {
+        return false;
+    }
+
+    void Component::AddChild(Component* child)
+    {
+
+    }
+
+    void Component::RemoveChild(Component* child)
+    {
+
+    }
+
+    Leaf::Leaf()
+    {
+    }
+    
+    Leaf::~Leaf()
+    {
+    }
+
+    std::string Leaf::Operation() {
+        return "Leaf";
+    }
+
+    Composite::Composite(/* args */)
+    {
+    }
+
+    Composite::~Composite()
+    {
+    }
+
+    bool Composite::IsParent() const
+    {
+        return true;
+    }
+    std::string Composite::Operation()
+    {
+        std::string result;
+        std::list<Component*>::iterator it = list_components.begin();
+
+        for (it=list_components.begin(); it != list_components.end(); ++it)
+        {
+            result += (*it)->Operation();
+        }
+        
+        return "Branch: ("+result+")";
+    }
+
+    void Composite::AddChild(Component* child)
+    {
+        list_components.push_back(child);
+        child->SetParent(this);
+    }
+
+    void Composite::RemoveChild(Component* child)
+    {
+        list_components.remove(child);
+        child->SetParent(nullptr);
+    }
+
+    Main::Main()
+    {
+    }
+    
+    Main::~Main()
+    {
+    }
+
+    void Main::execute()
+    {
+        Component* tree = new Composite();
+        Component* composite1 = new Composite();
+        Component* composite2 = new Composite();
+        Component* leaf1 = new Leaf(); 
+        Component* leaf2 = new Leaf(); 
+        Component* leaf3 = new Leaf(); 
+
+        composite1->AddChild(leaf1);
+        composite1->AddChild(leaf2);
+
+        std::cout<<"Composite1 "<<std::endl;
+        std::cout<<composite1->Operation()<<std::endl;
+
+        composite2->AddChild(leaf3);
+
+        std::cout<<"Composite2 "<<std::endl;
+        std::cout<<composite2->Operation()<<std::endl;
+
+        tree->AddChild(composite1);
+        tree->AddChild(composite2);
+
+        std::cout<<"tree "<<std::endl;
+        std::cout<<tree->Operation()<<std::endl;
+
+        delete tree;
+        delete composite1;
+        delete composite2;
+        delete leaf1;
+        delete leaf2;
+        delete leaf3;
+    }
+
 }
--- Structural/composite/composite.hpp
+++ Structural/composite/composite.hpp
@@ -1,9 +1,88 @@
-class Composite
+#include<iostream>
+#include<list>
+
+namespace CompositeDP
 {
-private:
-    /* data */
-public:
-    Composite(/* args */);
-    ~Composite();
-};
+    class Component
+    {
+    public:
+        Component();
+        Component(Component &&) = default;
+        Component(const Component &) = default;
+        Component &operator=(Component &&) = default;
+        Component &operator=(const Component &) = default;
+        ~Component();
+
+        void SetParent(Component *parent);
+
+        Component* GetParent(void) const;
+
+        virtual bool IsParent() const;
+        
+
+        virtual std::string Operation() = 0;
+
+        virtual void AddChild(Component* child);
+        virtual void RemoveChild(Component* child);
+
+    protected:
+        Component *parent;
+        
+    };
+
+    class Leaf: public Component
+    {
+    public:
+        Leaf();
+        Leaf(Leaf &&) = default;
+        Leaf(const Leaf &) = default;
+        Leaf &operator=(Leaf &&) = default;
+        Leaf &operator=(const Leaf &) = default;
+        ~Leaf();
+
+        std::string Operation() override;
+
+    
+    private:
+        
+    };
+    
+    class Composite: public Component
+    {
+    public:
+        Composite();
+        Composite(Composite &&) = default;
+        Composite(const Composite &) = default;
+        Composite &operator=(Composite &&) = default;
+        Composite &operator=(const Composite &) = default;
+        ~Composite();
+
+        bool IsParent() const;
+        
+
+        std::string Operation() override;
+
+        void AddChild(Component* child);
+        void RemoveChild(Component* child);
+    
+    private:
+        std::list<Component*> list_components;
+    };
+
+    class Main
+    {
+    public:
+        Main();
+        Main(Main &&) = default;
+        Main(const Main &) = default;
+        Main &operator=(Main &&) = default;
+        Main &operator=(const Main &) = default;
+        ~Main();
+
+        void execute();
+    
+    private:
+        
+    };
+}
 
